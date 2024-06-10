2fdff2bee2ee897ae1076093e4ebe41cf42a75e4
Airysm <shwon775@naver.com>
2024-06-10 15:47:49 +0900

    #8 - jpa test 작성
    기본 crud + 연관관계 매핑과 cascading이 잘 동작하는지 보려고 작성.
    빈 데이터만 확인하는 테스트

---
+++ fastcampus-project-board/src/test/java/com/example/fastcampus_project_board/repository/JpaRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.example.fastcampus_project_board.repository;
+
+import com.example.fastcampus_project_board.config.JpaConfig;
+import com.example.fastcampus_project_board.domain.Article;
+import com.example.fastcampus_project_board.domain.ArticleComment;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ActiveProfiles("testdb")
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // Given
+
+        // When
+        List<Article> articles = articleRepository.findAll();
+
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(0);
+    }
+}
